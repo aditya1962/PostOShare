1b40c6351cb478ff65227065a2ffe2f6d3a953df
aditya1962 <abeysingheaditya4@gmail.com>
2019-05-03 23:25:23 +0530

    Added build for production

---
+++ build/precache-manifest.cc886c92cb8ed4ff8bdba17df025a102.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3d0476c9701a36466ba7",
+    "url": "/PostOShare/static/js/runtime~main.2b19dce0.js"
+  },
+  {
+    "revision": "b45b662c01d538779e42",
+    "url": "/PostOShare/static/js/main.339d1ff2.chunk.js"
+  },
+  {
+    "revision": "f36fa19b377b7eb41f69",
+    "url": "/PostOShare/static/js/2.87b52ae0.chunk.js"
+  },
+  {
+    "revision": "b45b662c01d538779e42",
+    "url": "/PostOShare/static/css/main.9db91022.chunk.css"
+  },
+  {
+    "revision": "8ba263f2b2727935e918a3f783aaa08f",
+    "url": "/PostOShare/index.html"
+  }
+];
